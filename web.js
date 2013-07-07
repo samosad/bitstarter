38b8f1ae5de5d4cc7da0bffd944cc09ff2098080
Alexey Tabakman <samosad.ru@gmail.com>
2013-07-07 19:49:44 +0000

    Force web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readIndex = function() {
+  return fs.readFileSync("index.html").toString();
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(readIndex());
+  // response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
